40cd61a91f319a08f0c81ad3fe780a9d28e0dcc1
james0248 <hsjames0248@gmail.com>
2020-02-13 10:36:17 +0900

    Change searching method from DFS to BFS

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/phf/go-queue/queue"
 )
 
 type set map[string]bool
@@ -29,28 +30,41 @@ type ytChannel struct {
 	subscribers int
 	videos      []Video
 }
+type tuple struct {
+	url   string
+	depth int
+}
 
 var (
 	baseURL       = "https://www.youtube.com"
 	visited       = make(set)
 	relatedVideos = make([]Video, 0)
 	visitedMutex  = sync.RWMutex{}
+	videoQueue    = queue.New()
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	firstURL := scanner.Text()
-	ScrapeRelatedVideo(firstURL, 2)
+	videoQueue.PushBack(tuple{url: firstURL, depth: 2})
 
-	for _, vids := range relatedVideos {
-		fmt.Println(vids)
+	for videoQueue.Len() != 0 {
+		ScrapeRelatedVideo()
+	}
+
+	for _, videos := range relatedVideos {
+		fmt.Println(videos)
 	}
 }
 
-// ScrapeRelatedVideo scrapes all the related videos recursively
+// ScrapeRelatedVideo scrapes all the related videos by BFS
 // Sends its url through channel to check it is scraped sends empty string if depth is 0
-func ScrapeRelatedVideo(url string, depth int) {
+func ScrapeRelatedVideo() {
+	t := videoQueue.PopFront()
+	url := t.(tuple).url
+	depth := t.(tuple).depth
+
 	_, ok := visited[url]
 	if ok || depth <= 0 {
 		return
@@ -58,9 +72,8 @@ func ScrapeRelatedVideo(url string, depth int) {
 	visited[url] = true
 
 	res, err := http.Get(url)
-	checkRes(res)
 	checkErr(err)
-	defer res.Body.Close()
+	checkRes(res)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
@@ -69,9 +82,10 @@ func ScrapeRelatedVideo(url string, depth int) {
 		Each(func(index int, s *goquery.Selection) {
 			s.Find("a.content-link").Each(func(index int, link *goquery.Selection) {
 				nextLink, _ := link.Attr("href")
-				ScrapeRelatedVideo(baseURL+nextLink, depth-1)
+				videoQueue.PushBack(tuple{url: baseURL + nextLink, depth: depth - 1})
 			})
 		})
+	res.Body.Close()
 
 }
 
@@ -111,12 +125,10 @@ func checkVisit(url string) {
 	if len(url) == 0 {
 		return
 	}
-	visitedMutex.RLock()
 	if visit, ok := visited[url]; !visit || !ok {
 		fmt.Println(visit, ok)
-		log.Fatalln("Video is not scraped while visiting", url)
+		panic("Video is not scraped while visiting" + url)
 	}
-	visitedMutex.RUnlock()
 }
 
 func checkRes(res *http.Response) {
@@ -127,6 +139,6 @@ func checkRes(res *http.Response) {
 
 func checkErr(err error) {
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 }
